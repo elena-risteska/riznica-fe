792aa58419942d1dd1b90aceb104e18b4d07f786
elena-risteska <elena.risteska003@gmail.com>
2025-07-23 13:32:16 +0200

    set up MUI

---
+++ src/theme.js
@@ -0,0 +1,41 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import "@fontsource/mulish";
+
+let theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#FFCC00",
+    },
+    secondary: {
+      main: "#EB5B00",
+    },
+    info: {
+      main: "#B12C00",
+    },
+    background: {
+      default: "#fafafa",
+    },
+  },
+  typography: {
+    fontFamily: "Mulish, sans-serif",
+    h1: {
+      fontWeight: 700,
+      fontSize: "3rem",
+    },
+    h2: {
+      fontWeight: 600,
+      fontSize: "2.25rem",
+    },
+    body1: {
+      fontSize: "1rem",
+    },
+    button: {
+      textTransform: "none",
+      fontWeight: 600,
+    },
+  },
+});
+theme = responsiveFontSizes(theme);
+
+export default theme;
