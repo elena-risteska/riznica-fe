8d8a7386994418777d0a616345a85d9f549e8a92
elena-risteska <elena.risteska003@gmail.com>
2025-07-31 15:03:39 +0200

    make assets folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
-import theme from "../src/config/theme.js";
+import theme from "../src/assets/theme.js";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
