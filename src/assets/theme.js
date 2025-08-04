4451ac9c1bd89a737a64dc251b713af5753752a2
elena-risteska <elena.risteska003@gmail.com>
2025-08-04 21:04:31 +0200

    style about us page

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -21,11 +21,15 @@ let theme = createTheme({
     fontFamily: "Mulish, sans-serif",
     h1: {
       fontWeight: 700,
-      fontSize: "3rem",
+      fontSize: "2.5rem",
     },
     h2: {
       fontWeight: 600,
       fontSize: "2.25rem",
+    },
+    h3: {
+      fontWeight: 600,
+      fontSize: "2rem",
     },
     h4: {
       fontWeight: 600,
@@ -34,6 +38,10 @@ let theme = createTheme({
     h5: {
       fontWeight: 600,
       fontSize: "1.25rem",
+    },
+    h6: {
+      fontWeight: 600,
+      fontSize: "1rem",
     },
     body1: {
       fontSize: "1rem",
